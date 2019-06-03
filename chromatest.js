36ec10d70adb347dba562b8f86c732e3c5d822ce
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-06-03 21:20:59 +0900

    0603

--- chromatest.js
+++ chromatest.js
@@ -1,10 +1,15 @@
+var finresult=[];
+var finresult2=[];
+var j;
+
+
 var video2 = document.getElementById('video2');
     // video2は非表示にしておく
     video2.style.display = 'none';
 
     var canvas2 = document.getElementById('canvas2');
     // そのまま表示すると鏡像にならないので反転させておく
-    canvas2.style.transform = 'rotateY(180deg)';
+    video2.style.transform = 'rotateY(180deg)';
 
     var context = canvas2.getContext('2d');
 
@@ -15,24 +20,34 @@ var video2 = document.getElementById('video2');
   //aspectRatio: {exact: 1.7777777778},
   width: 1280,height:720}, audio: false}, function (stream) {
         video2.srcObject = stream;
+
+
         draw();
+
+
+        //draw();
     }, function () {});
 
     // video2の映像をcanvas2に描画する
     var draw = function () {
-        context.drawImage(video2, 0, 0, canvas2.width,canvas2.height/2,0, 0, canvas2.width, canvas2.height/2);
+        context.drawImage(video2, 0, 0, canvas2.width,canvas2.height,0, 0, canvas2.width, canvas2.height);
         // ここでクロマキー処理をする
         chromaKey();
         requestAnimationFrame(draw);
+        if(j== null){return}
+        console.log(j);
+       document.getElementById("res").innerHTML=j;
+        j=null;
     };
 
     // 消す色と閾値
     var chromaKeyColor = {r: 0, g: 255, b: 0},
-        colorDistance = 200;
+        colorDistance = 500;
 
     // クロマキー処理
-    var chromaKey = function () {
-        var imageData = context.getImageData(0, 0, canvas2.width, canvas2.height/2),
+    var chromaKey = function ()
+    {
+        var imageData = context.getImageData(0, 0, canvas2.width, canvas2.height),
             data = imageData.data; //参照渡し
 
         // dataはUint8ClampedArray
@@ -41,7 +56,8 @@ var video2 = document.getElementById('video2');
         // 右隣のピクセルのr,g,b,aの値が続く
         // n から n+4 までが1つのピクセルの情報となる
 
-        for (var i = 0, l = data.length; i < l; i += 4) {
+        for (var i = 0, l = data.length; i < l; i += 64)
+        {
             var target = {
                     r: data[i],
                     g: data[i + 1],
@@ -54,7 +70,21 @@ var video2 = document.getElementById('video2');
                 // alpha値を0にすることで見えなくする
                 data[i + 3] = 0;
             }
+            else
+            {
+            finresult.push(i);
+            j=finresult.reduce((a,b)=>Math.min(a,b));
+            setTimeout(function(){finresult.length=0;},5000);
+            }
+            /*var j=finresult.reduce((a,b)=>Math.min(a,b));
+            finresult2.push(j);
+            var k=finresult2.join(',');
+            console.log(k); */
+
         }
+
+           //finresult.push(i);
+           //console.log(finresult.reduce((a,b)=>Math.min(a,b)));
 
         context.putImageData(imageData, 0, 0);
     };
@@ -84,8 +114,33 @@ var video2 = document.getElementById('video2');
         };
     };
 
+
+
+
+
     var distance = document.getElementById('distance');
     distance.style.textAlign = 'right';
     distance.addEventListener('change', function () {
         colorDistance = this.value;
+    });
+
+//canvasの縦横、位置をUIで変化させる。
+    canvas2Wid.style.textAlign = 'right';
+    canvas2Wid.addEventListener('change', function () {
+        document.getElementById("canvas2").width = this.value;
+    });
+
+        canvas2Hei.style.textAlign = 'right';
+    canvas2Hei.addEventListener('change', function () {
+        document.getElementById("canvas2").height = this.value;
+    });
+
+            canvas2X.style.textAlign = 'right';
+    canvas2X.addEventListener('change', function () {
+        document.getElementById("canvas2").style.left = this.value + "px";
+    });
+
+            canvas2Y.style.textAlign = 'right';
+    canvas2Y.addEventListener('change', function () {
+        document.getElementById("canvas2").style.top = this.value + "px";
     });
